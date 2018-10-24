61deb4b8a36554aab9848433b4e32daf77be0387
Marcelo <marcelosvh@hotmail.com>
2018-10-24 16:53:00 -0500

    Model Administrador

---
+++ Models/Administrador.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace PROYECTO_APP_VISION_VISUAL_STUDIO {
+
+    public class Administrador {
+
+        public string dni {get; set;}
+        public string nombres {get; set;}
+        public string apepat {get; set;}
+        public string apemat {get; set;}
+        public string contra {get; set;}
+        public string nivel {get; set;}
+
+    }
+
+}
