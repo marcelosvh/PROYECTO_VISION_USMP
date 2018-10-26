14c7523f772a31853047be423df4454a1b687c8d
marynadi123456 <marynadi07@gmail.com>
2018-10-26 14:50:26 -0500

    Models Evento, Expositor

--- Models/Evento.cs
+++ Models/Evento.cs
@@ -4,9 +4,8 @@ namespace PROYECTO_APP_VISION_VISUAL_STUDIO.Models
 {
     public class Evento
     {
-        public int cod_tip_doc {get; set;}
-        public int cod_expositor {get; set;}
-        public int numero {get; set;}
+        public int cod_eve {get; set;}
+        public string tema {get; set;}
 
     }
 }
---
+++ Models/Expositor.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace PROYECTO_APP_VISION_VISUAL_STUDIO.Models
+{
+    public class Expositor
+    {
+        public int cod_exp {get; set;}
+        public string ape_pat {get; set;}
+        public string ape_mat {get; set;}
+        public string nom {get; set;}
+        public string nacion {get; set;}
+        public string tip_ora {get; set;}
+        public string correo {get; set;}
+        public int orden {get; set;}
+        public int cod_empresa {get; set;}
+        
+
+
+    }
+}
