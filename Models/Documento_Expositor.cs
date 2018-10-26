45df33fb3b6caa02a5e265f4615ebf91f2358425
Marynadi <marynadi07@gmail.com>
2018-10-26 14:10:38 -0500

    Model documento expositor

---
+++ Models/Documento_Expositor.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PROYECTO_APP_VISION_VISUAL_STUDIO.Models
+{
+    public class Documento_Expositor
+    {
+        public int cod_tip_doc {get; set;}
+        public int cod_expositor {get; set;}
+        public int numero {get; set;}
+
+    }
+}
