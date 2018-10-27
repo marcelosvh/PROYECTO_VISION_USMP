ff4b6ce2cf61f70e3eee32e0646a6b54d8ddd07e
Marcelo <marcelosvh@hotmail.com>
2018-10-27 17:45:50 -0500

    model carrera terminado

---
+++ Models/Carrera.cs
@@ -0,0 +1,9 @@
+
+using System;
+namespace PROYECTO_APP_VISION_VISUAL_STUDIO {
+    public class Carrera1{
+        public String idCarrera{get;set;}
+        public String carrera{get;set;}
+
+    }
+} 
