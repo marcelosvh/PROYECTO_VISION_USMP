389d1257b021ff1a8bd31d5e2469a89b65f6a2e2
Marcelo <marcelosvh@hotmail.com>
2018-10-27 17:50:22 -0500

    model empresa terminado

---
+++ Models/Empresa.cs
@@ -0,0 +1,12 @@
+
+using System;
+namespace PROYECTO_APP_VISION_VISUAL_STUDIO {
+    public class Empresa{
+        public String idEmpresa{get;set;}
+        public String empresa {get;set;}
+        public String correo {get;set;}
+        public String telefono {get;set;}
+        public String pais {get;set;}
+        
+    }
+} 
