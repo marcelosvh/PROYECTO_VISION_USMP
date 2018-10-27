eb797c08e5057c941e9bea743633f86d34f0ec06
Marcelo <marcelosvh@hotmail.com>
2018-10-27 17:54:28 -0500

    model conferencia terminado

---
+++ Models/Conferencia.cs
@@ -0,0 +1,12 @@
+
+using System;
+namespace PROYECTO_APP_VISION_VISUAL_STUDIO {
+    public class Conferencia{
+        public String idConferencia{get;set;}
+        public String conferencia{get;set;}
+        public String horadeinicio{get;set;}
+        public String horadefin{get;set;}
+    
+        
+    }
+} 
