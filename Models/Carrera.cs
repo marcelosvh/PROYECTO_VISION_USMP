1371d9898cb20eeba3b775803963cbd605ef55a5
Allison Acosta <allison_acosta1@usmp.pe>
2018-11-03 16:28:50 -0500

    Corregir Carrera

--- Models/Carrera.cs
+++ Models/Carrera.cs
@@ -1,7 +1,7 @@
 
 using System;
 namespace PROYECTO_APP_VISION_VISUAL_STUDIO {
-    public class Carrera1{
+    public class Carrera{
         public String idCarrera{get;set;}
         public String carrera{get;set;}
 
