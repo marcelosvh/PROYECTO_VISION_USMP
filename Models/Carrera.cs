11a9a5d19ecca993229ea95151df17ffdc2da092
Allison Acosta <allison_acosta1@usmp.pe>
2018-11-03 16:31:22 -0500

    Carrera

--- Models/Carrera.cs
+++ Models/Carrera.cs
@@ -1,9 +1,9 @@
-
 using System;
+
 namespace PROYECTO_APP_VISION_VISUAL_STUDIO {
-    public class Carrera{
-        public String idCarrera{get;set;}
-        public String carrera{get;set;}
+    public class Carrera {
+        public string idCarrera{get;set;}
+        public string carrera{get;set;}
 
     }
 } 
