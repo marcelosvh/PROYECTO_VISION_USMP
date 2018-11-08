f3f6c0c71c3d35675326c6d3908721e0da5ad641
Marynadi <marynadi07@gmail.com>
2018-11-08 18:40:16 -0500

    commit

--- Controllers/Documento_Expositor.cs
+++ Controllers/Documento_Expositor.cs
@@ -44,4 +44,31 @@ namespace PROYECTO_APP_VISION_VISUAL_STUDIO.Controllers
             return View(expTipDoc);
         }
 
-        
+         // GET: ExpTipDoc/Create
+        public IActionResult Create()
+        {
+            ViewData["IDExpositor"] = new SelectList(_context.Expositor, "IDExpositor", "IDExpositor");
+            ViewData["IDTip"] = new SelectList(_context.Set<TipoDocumento>(), "IDTip", "IDTip");
+            return View();
+        }
+
+        // POST: ExpTipDoc/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IDTip,IDExpositor,Número")] ExpTipDoc expTipDoc)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(expTipDoc);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IDExpositor"] = new SelectList(_context.Expositor, "IDExpositor", "IDExpositor", expTipDoc.IDExpositor);
+            ViewData["IDTip"] = new SelectList(_context.Set<TipoDocumento>(), "IDTip", "IDTip", expTipDoc.IDTip);
+            return View(expTipDoc);
+        }
+
+
+
