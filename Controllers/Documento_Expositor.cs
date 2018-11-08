2802ec9d434d61e9fe391d777d3ffea2c821a4b2
Marynadi <marynadi07@gmail.com>
2018-11-08 18:22:26 -0500

    Clase expdoc

--- Controllers/Documento_Expositor.cs
+++ Controllers/Documento_Expositor.cs
@@ -1,112 +1,19 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using PROYECTO_APP_VISION_VISUAL_STUDIO.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PROYECTO_VISION_USMP.Models;
 
 namespace PROYECTO_APP_VISION_VISUAL_STUDIO.Controllers
 {
-    public class Documento_ExpositorController : Controller
+    public class ExpTipDocController : Controller
     {
-        VisionDataAccessLayer objadmin = new VisionDataAccessLayer();
+        private readonly MvcContext _context;
 
-        public IActionResult Index()
+        public ExpTipDocController(MvcContext context)
         {
-            List<Documento_Expositor> lstDoc_exp = new List<Documento_Expositor>();
-            lstDoc_exp = objdoc_exp.GetAllDocumento_Expositor().ToList();
-
-            return View(lstDoc_exp);
+            _context = context;
         }
-        [HttpGet]
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create([Bind] Documento_Expositor documento_expositor)
-        {
-            if (ModelState.IsValid)
-            {
-                objadmin.AddDocumento_Expositor(documento_expositor);
-                return RedirectToAction("Index");
-            }
-            return View(documento_expositor);
-        }
-
-        [HttpGet]
-        public IActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-            Documento_Expositor documento_expositor = objadmin.GetDocumento_ExpositorData(id);
-
-            if (documento_expositor == null)
-            {
-                return NotFound();
-            }
-            return View(documento_expositor);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(int id, [Bind] Documento_Expositor documento_expositor)
-        {
-            if (id != documento_expositor.ID)
-            {
-                return NotFound();
-            }
-            if (ModelState.IsValid)
-            {
-                objadmin.UpdateDocumento_Expositor(documento_expositor);
-                return RedirectToAction("Index");
-            }
-            return View(documento_expositor);
-        }
-
-        [HttpGet]
-        public IActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-            Documento_Expositor documento_expositor = objadmin.GetDocumento_ExpositorrData(id);
-
-            if (documento_expositor == null)
-            {
-                return NotFound();
-            }
-            return View(documento_expositor);
-        }
-
-        [HttpGet]
-        public IActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-            Documento_Expositor documento_expositor = objadmin.GetDocumento_ExpositorData(id);
-
-            if (documento_expositor == null)
-            {
-                return NotFound();
-            }
-            return View(documento_expositor);
-        }
-
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeleteConfirmed(int? id)
-        {
-            objadmin.DeleteDocumento_Expositor(id);
-            return RedirectToAction("Index");
-        }
-    }
-}
