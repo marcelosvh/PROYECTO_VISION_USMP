b8778979e9629dfc6944f1d19d526b23860abc58
Marynadi <marynadi07@gmail.com>
2018-11-08 18:43:20 -0500

    documento_expositor

--- Controllers/Documento_Expositor.cs
+++ Controllers/Documento_Expositor.cs
@@ -88,4 +88,40 @@ namespace PROYECTO_APP_VISION_VISUAL_STUDIO.Controllers
             return View(expTipDoc);
         }
 
+        // POST: ExpTipDoc/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("IDTip,IDExpositor,Número")] ExpTipDoc expTipDoc)
+        {
+            if (id != expTipDoc.IDTip)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(expTipDoc);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ExpTipDocExists(expTipDoc.IDTip))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IDExpositor"] = new SelectList(_context.Expositor, "IDExpositor", "IDExpositor", expTipDoc.IDExpositor);
+            ViewData["IDTip"] = new SelectList(_context.Set<TipoDocumento>(), "IDTip", "IDTip", expTipDoc.IDTip);
+            return View(expTipDoc);
+        }
 
