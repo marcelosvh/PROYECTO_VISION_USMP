a63486fd369b0db06188a8e253b3d6fd6c7fe911
Marcelo <marcelosvh@hotmail.com>
2018-11-09 12:11:29 -0500

    Task Details AulaController

--- Controllers/AulaController.cs
+++ Controllers/AulaController.cs
@@ -25,6 +25,24 @@ namespace PROYECTO_APP_VISION_VISUAL_STUDIO.Controllers
             return View(await _context.Aula.ToListAsync());
         }
 
+        // GET: Aula/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aula = await _context.Aula
+                .SingleOrDefaultAsync(m => m.IDAula == id);
+            if (aula == null)
+            {
+                return NotFound();
+            }
+
+            return View(aula);
+        }
+
     }
 
 }
