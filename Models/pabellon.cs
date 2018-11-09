58fc5d3d093ce56147be04210a5af7c9b4125c5b
Jeanpiere <jeanpiere92_28@hotmail.com>
2018-11-09 09:51:50 -0500

    Pabellon.1.2

--- Models/pabellon.cs
+++ Models/pabellon.cs
@@ -2,3 +2,9 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PROYECTO_VISION_USMP.Models 
+{ 
+    public class Pabellon
+ 
+    { 
