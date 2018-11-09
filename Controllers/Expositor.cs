358209422d580c6765cc630ff477081a5e968a22
Marynadi <marynadi07@gmail.com>
2018-11-08 20:24:11 -0500

    POST: Expositor/Create

--- Controllers/Expositor.cs
+++ Controllers/Expositor.cs
@@ -51,3 +51,20 @@ namespace PROYECTO_APP_VISION_VISUAL_STUDIO.Controllers
             return View();
         }
 
+        // POST: Expositor/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IDExpositor,ApePat,ApeMat,NomExp,Nacionalidad,TipoOrador,CorreoExp,Orden,IDEmpresa")] Expositor expositor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(expositor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IDEmpresa"] = new SelectList(_context.Empresa, "IDEmpresa", "IDEmpresa", expositor.IDEmpresa);
+            return View(expositor);
+        }
+
