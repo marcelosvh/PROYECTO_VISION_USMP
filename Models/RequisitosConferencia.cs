69fa5027b5ae05a633a556677831541301b96ccf
Jeanpiere <jeanpiere92_28@hotmail.com>
2018-11-09 10:04:38 -0500

    RequisitosConferencia.1.2

--- Models/RequisitosConferencia.cs
+++ Models/RequisitosConferencia.cs
@@ -3,3 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations; 
  
 namespace PROYECTO_VISION_USMP.Models 
+{ 
+    public class RequisitosConferencia
+ 
+    { 
--- Models/Requisitos_Conferencia.cs
+++
@@ -1,5 +0,0 @@
-using System;   
-using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations; 
- 
-namespace PROYECTO_VISION_USMP.Models 
