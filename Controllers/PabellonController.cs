5c208004b0ddc3da5af46e1ae1480d9d52484630
Jeanpiere <jeanpiere92_28@hotmail.com>
2018-11-08 20:12:14 -0500

    PabellonController.1.2

--- Controllers/PabellonController.cs
+++ Controllers/PabellonController.cs
@@ -6,3 +6,38 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using PROYECTO_VISION_USMP.Models;
+
+namespace PROYECTO_APP_VISION_VISUAL_STUDIO.Controllers
+{
+    public class PabellonController : Controller
+    {
+        private readonly MvcContext _context;
+
+        public PabellonController(MvcContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pabellon
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Pabellon.ToListAsync());
+        }
+
+        // GET: Pabellon/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pabellon = await _context.Pabellon
+                .SingleOrDefaultAsync(m => m.IDPabellon == id);
+            if (pabellon == null)
+            {
+                return NotFound();
+            }
+
+            return View(pabellon);
+        }
