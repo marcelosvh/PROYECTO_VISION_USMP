54135445aa1ea7e2481cf651406e884414f05a22
Allison <allison_acosta1@usmp.pe>
2018-11-09 11:18:05 -0500

    Creacion EmpresaController

---
+++ Controllers/EmpresaController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PROYECTO_VISION_USMP.Models;
+
+namespace PROYECTO_APP_VISION_VISUAL_STUDIO.Controllers
+{
+    public class EmpresaController : Controller
+    {
+        
+    }
+}
