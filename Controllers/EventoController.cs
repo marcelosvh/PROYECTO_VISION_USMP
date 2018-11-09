4bf784c4a19d1bc43a651dd55ce4bbd807207a78
marynadi <marynadi07@gmail.com>
2018-11-09 09:01:52 -0500

    asdasd

--- Controllers/EventoController.cs
+++ Controllers/EventoController.cs
@@ -6,3 +6,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using PROYECTO_VISION_USMP.Models;
+
+namespace PROYECTO_APP_VISION_VISUAL_STUDIO.Controllers
+{
+    public class EventoController : Controller
+    {
+        private readonly MvcContext _context;
+
+        public EventoController(MvcContext context)
+        {
+            _context = context;
+        }
