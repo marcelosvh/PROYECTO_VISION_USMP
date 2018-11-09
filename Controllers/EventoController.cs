da56a5f889de35ec6add5acfe5aa35d81077252d
marynadi <marynadi07@gmail.com>
2018-11-09 09:08:10 -0500

    delete

--- Controllers/EventoController.cs
+++ Controllers/EventoController.cs
@@ -113,3 +113,21 @@ namespace PROYECTO_APP_VISION_VISUAL_STUDIO.Controllers
             }
             return View(evento);
         }
+
+        // GET: Evento/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var evento = await _context.Evento
+                .SingleOrDefaultAsync(m => m.IDEvento == id);
+            if (evento == null)
+            {
+                return NotFound();
+            }
+
+            return View(evento);
+        }
