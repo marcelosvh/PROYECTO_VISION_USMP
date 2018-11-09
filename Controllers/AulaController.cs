af0a6d77bf514a560ba3cc014bac46c3a1bd7002
Marcelo <marcelosvh@hotmail.com>
2018-11-09 12:23:38 -0500

    Task Delete AulaController

--- Controllers/AulaController.cs
+++ Controllers/AulaController.cs
@@ -116,6 +116,24 @@ namespace PROYECTO_APP_VISION_VISUAL_STUDIO.Controllers
             return View(aula);
         }
 
+        // GET: Aula/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aula = await _context.Aula
+                .SingleOrDefaultAsync(m => m.IDAula == id);
+            if (aula == null)
+            {
+                return NotFound();
+            }
+
+            return View(aula);
+        }
+
     }
 
 }
