f9e078b4602372d92cb7dca77e00f97616577113
Allison <allison_acosta1@usmp.pe>
2018-11-09 10:10:40 -0500

    creacion de carreracontroller

---
+++ Controllers/CarreraController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PROYECTO_VISION_USMP.Models;
+
+namespace PROYECTO_APP_VISION_VISUAL_STUDIO.Controllers
+{
+    public class CarreraController : Controller
+    {
+
+    }
+
+}
