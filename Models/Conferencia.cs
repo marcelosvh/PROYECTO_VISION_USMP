aea74d93c99853481013e025963acc21fd809b1d
Marcelo <marcelosvh@hotmail.com>
2018-11-10 17:49:11 -0500

    Model Conferencia.cs

--- Models/Conferencia.cs
+++ Models/Conferencia.cs
@@ -1,4 +1,7 @@
-using System;
+using System;  
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PROYECTO_APP_VISION_VISUAL_STUDIO {
     public class Conferencia {
