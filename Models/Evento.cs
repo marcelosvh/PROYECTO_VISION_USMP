c4cb4acb67fbe8d41f5e3b991539e42035b97dd2
Marcelo <marcelosvh@hotmail.com>
2018-11-10 18:28:56 -0500

    Evento.cs

--- Models/Evento.cs
+++ Models/Evento.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using PROYECTO_APP_VISION_VISUAL_STUDIO;
+
 namespace PROYECTO_VISION_USMP.Models
 
 {
