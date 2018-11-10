2e7109a2df9f5b511c7686d652c492231885153c
Jose Miguel <chela_16_10@hotmail.com>
2018-11-09 21:26:51 -0500

    Taller.cs

--- Models/Taller.cs
+++ Models/Taller.cs
@@ -1,29 +1,46 @@
 using System;  
 using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
-
-
-namespace PROYECTO_APP_VISION_VISUAL_STUDIO.Models
+namespace PROYECTO_VISION_USMP.Models
 {
     public class Taller
     {
-        
-        public int Idtaller { get; set; }
+        [Key]
+        [Required]
+        [Display(Name="Codigo de Taller")]
+        public string IDTaller { get; set; }
+        [ForeignKey("IDTaller")]
+        [Display(Name="Nombre de Taller")]
         [Required]
         public string NomTaller { get; set; }
+        [Display(Name="Hora de Inicio ")]
+        [DisplayFormat(DataFormatString = "{0:HH:mm}", ApplyFormatInEditMode=true)]
+        [DataType(DataType.Time)]
         [Required]
-        public string Hora_ini { get; set; }
+        public DateTime HoraIni { get; set; }
+        [Display(Name="Hora de Fin")]
+        [DisplayFormat(DataFormatString = "{0:HH:mm}", ApplyFormatInEditMode=true)]
+        [DataType(DataType.Time)]
         [Required]
-        public string Hora_fin { get; set; }
+        public DateTime HoraFin { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode=true)]
+        [DataType(DataType.Date)]
         [Required]
         public string Fecha { get; set; }
-        [Required]
-        public string Idpab { get; set; }
-        [Required]
-        public string Idaula { get; set; }
-        [Required]
-        public string Idcarrera { get; set; }
-        [Required]
-        public string Idevento { get; set; }
+        public string IDPabellon {get; set;}
+        
+        public string IDAula { get; set; }
+        
+        public string IDCarrera { get; set; }
+        
+        public string IDEvento { get; set; }
+        
+        public virtual Pabellon CodigoPabellon {get; set;}
+        public virtual Aula CodigoAula { get; set;}
+        public virtual Carrera CodigoCarrera {get; set;}
+        public virtual Evento CodigoEvento {get; set;}
+        
     }
 }
