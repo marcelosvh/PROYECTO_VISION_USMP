8fee6ba25d24ceb5014326eb0c49bd39b11b0b84
Marcelo <marcelosvh@hotmail.com>
2018-11-10 15:30:02 -0500

    Creación Model Alumno.cs

---
+++ Models/Alumno.cs
@@ -0,0 +1,13 @@
+using System;  
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace PROYECTO_VISION_USMP.Models
+{
+    
+    public class Alumno
+    {
+
+    }
+    
+}
