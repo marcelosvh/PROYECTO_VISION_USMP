74e09f5bc04bb14b19c188dfc18d8c19b351983b
Jose Miguel <chela_16_10@hotmail.com>
2018-11-09 22:46:57 -0500

    Create ReqTallerController

--- Controllers/RequisitosTallerController.cs
+++ Controllers/RequisitosTallerController.cs
@@ -44,6 +44,32 @@ namespace PROYECTO_APP_VISION_VISUAL_STUDIO.Controllers
             return View(requisitosTaller);
         }
 
+        // GET: RequisitosTaller/Create
+        public IActionResult Create()
+        {
+            ViewData["IDExpositor"] = new SelectList(_context.Expositor, "IDExpositor", "IDExpositor");
+            ViewData["IDTaller"] = new SelectList(_context.Set<Taller>(), "IDTaller", "IDTaller");
+            return View();
+        }
+
+        // POST: RequisitosTaller/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IDExpositor,IDTaller,DescReqTal")] RequisitosTaller requisitosTaller)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(requisitosTaller);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IDExpositor"] = new SelectList(_context.Expositor, "IDExpositor", "IDExpositor", requisitosTaller.IDExpositor);
+            ViewData["IDTaller"] = new SelectList(_context.Set<Taller>(), "IDTaller", "IDTaller", requisitosTaller.IDTaller);
+            return View(requisitosTaller);
+        }
+
         
     }
 }
