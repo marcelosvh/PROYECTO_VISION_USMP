cd499c89dc455cbb608140b2b849d5913ebda1ac
Allison Acosta <allison_acosta1@usmp.pe>
2018-11-10 16:11:50 -0500

    correccion de carera.cs

--- Models/Carrera.cs
+++ Models/Carrera.cs
@@ -2,7 +2,17 @@ using System;
 
 namespace PROYECTO_APP_VISION_VISUAL_STUDIO {
     public class Carrera {
-        public string idcarrera {get;set;}
-        public string carrera {get;set;}
+       [Key]
+        [Required]
+        [Display(Name="Codigo de Carrera")]
+        
+        public string IDCarrera {get; set;}
+        [Display(Name="Nombre de Carrera")]
+        [Required]
+        public string NomCarrera { get; set;}
+        public ICollection<Taller> Talleres {get; set;}
+        public ICollection<Alumno> Alumnos {get; set;}
+        public ICollection<Conferencia> Conferencias {get; set;}
     }
+
 }
