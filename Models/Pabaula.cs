e18442febdd2f603057ff5722d1210c60dc8c2d8
Marcelo <marcelosvh@hotmail.com>
2018-11-10 18:29:36 -0500

    Pabaula.cs

--- Models/Pabaula.cs
+++ Models/Pabaula.cs
@@ -2,6 +2,8 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using PROYECTO_APP_VISION_VISUAL_STUDIO.Models;
+
 namespace PROYECTO_VISION_USMP.Models 
 { 
 
@@ -19,7 +21,7 @@ namespace PROYECTO_VISION_USMP.Models
 
          [Required]
         public int Capacidad {get; set;}
-        public virtual Pabellon CodigoPabellon {get; set;}
+        public virtual pabellon CodigoPabellon {get; set;}
         public virtual Aula CodigoAula {get;set;}
        
     
